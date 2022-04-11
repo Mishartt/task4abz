bc34fbc52a6cb6387546b7053dfa097c37a5f3b8
Mishartt <96066793+Mishartt@users.noreply.github.com>
2022-04-11 11:06:59 +0300

    add tooltip

--- src/Components/UserItem.jsx
+++ src/Components/UserItem.jsx
@@ -2,14 +2,19 @@ import React from "react";
 
 const UserItem = ({user}) => {
     
-   
 
     return(
         <div className="user__item">
             <img src={user.photo}/>
              <p className="user__name">{user.name}</p>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
+                <p>{user.position}</p>
+
+               <div><p className="user__email">{user.email.length > 25 ? `${user.email.substring(0,25)}...` : user.email}</p>
+                    <div style={{display:user.email.length > 25 ? '' : 'none'}} className="full__email">
+                        <p>{user.email}</p>
+                    </div>
+               </div>
+
                 <p>{user.phone}</p>
         </div>
     )
